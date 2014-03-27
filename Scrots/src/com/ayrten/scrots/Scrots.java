a067d585d94913e7ba2fab55387d76f697559cbf
Andy Yeung <nsdfer@gmail.com>
2014-03-26 22:23:28 -0700

    Played around with it
    Click/Press on screen and the image will move diag

--- Scrots/src/com/ayrten/scrots/Scrots.java
+++ Scrots/src/com/ayrten/scrots/Scrots.java
@@ -9,58 +9,64 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
-public class Scrots implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+public class Scrots implements ApplicationListener
+{
+	private SpriteBatch	batch;
+	private Texture		texture;
+	TextureRegion		region;
+	Vector2				position;
 	
 	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
+	public void create()
+	{
 		batch = new SpriteBatch();
 		
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		region = new TextureRegion(texture, 0, 0, 50, 50);
 		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		position = new Vector2(50,50);
 	}
-
+	
 	@Override
-	public void dispose() {
+	public void dispose()
+	{
 		batch.dispose();
 		texture.dispose();
 	}
-
+	
 	@Override
-	public void render() {		
+	public void render()
+	{
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
+		if (Gdx.input.isTouched())
+		{
+			position.x += 1f;
+			position.y += 1f;
+		}
+		
 		batch.begin();
-		sprite.draw(batch);
+		batch.draw(region, position.x, position.y);
 		batch.end();
 	}
-
+	
 	@Override
-	public void resize(int width, int height) {
+	public void resize(int width, int height)
+	{
 	}
-
+	
 	@Override
-	public void pause() {
+	public void pause()
+	{
 	}
-
+	
 	@Override
-	public void resume() {
+	public void resume()
+	{
 	}
 }
