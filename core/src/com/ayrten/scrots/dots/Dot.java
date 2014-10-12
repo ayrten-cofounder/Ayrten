16b84fb258f0bbfd161e487b62a5f5c446f7a153
Andy Yeung <nsdfer@gmail.com>
2014-10-12 02:21:56 -0700

    CHANGED THE GAME
    beautiful

--- core/src/com/ayrten/scrots/dots/Dot.java
+++ core/src/com/ayrten/scrots/dots/Dot.java
@@ -1,5 +1,7 @@
 package com.ayrten.scrots.dots;
 
+import java.util.Random;
+
 import com.ayrten.scrots.manager.Manager;
 import com.ayrten.scrots.screens.ScrotsGame;
 import com.badlogic.gdx.Gdx;
@@ -11,38 +13,39 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 
-public class Dot extends Actor
-{
-	private static final float SIZE_RATIO = 15;
-	
+public class Dot extends Actor {
+	private static final int BUMP_IT_UP = 50;
+	private static final float MAX_SIZE_RATIO = 30; // No use really...
+	private float size_ratio = MAX_SIZE_RATIO / 2; // arbituary
+													// buffer...jk....is min
+													// size
+
 	private Texture dot;
-//	private NinePatch dot;
+	// private NinePatch dot;
 
 	public Manager gm;
+	private Random random;
 
 	public Sound pop;
 
-	public Dot(Texture dot, Manager gm, Sound pop)
-	{
+	public Dot(Texture dot, Manager gm, Sound pop) {
 		this.dot = dot;
 		this.gm = gm;
 		this.pop = pop;
+		random = new Random(System.currentTimeMillis());
 		setBounds(getX(), getY(), dot.getWidth(), dot.getHeight());
 		// An InputListener is a subclass of EventListener
-		addListener(new InputListener()
-		{
+		addListener(new InputListener() {
 			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button)
-			{
+					int pointer, int button) {
 				return true;
 			}
 
 			public void touchUp(InputEvent event, float x, float y,
-					int pointer, int button)
-			{
+					int pointer, int button) {
 
 				// Gotta get touched by an angel ;)
-				  touchedByAnAngel();
+				touchedByAnAngel();
 
 				// Remove the actor from the stage.
 				event.getTarget().remove();
@@ -50,44 +53,40 @@ public class Dot extends Actor
 		});
 	}
 
-	private float getCircleWidth()
-	{
-		return (float) (gm.w / SIZE_RATIO);
+	private float getCircleWidth() {
+		return (float) (gm.w / size_ratio);
 	}
 
-	private float getCircleHeight()
-	{
-		return (float) (gm.w / SIZE_RATIO);
+	private float getCircleHeight() {
+		return (float) (gm.w / size_ratio);
 	}
 
 	// This class shall be overriddent by the blue, green, red dots
-	public void touchedByAnAngel()
-	{
-		if(((ScrotsGame) Gdx.app.getApplicationListener()).prefs.getBoolean("sound_effs", true))
-		  pop.play();
+	public void touchedByAnAngel() {
+		if (((ScrotsGame) Gdx.app.getApplicationListener()).prefs.getBoolean(
+				"sound_effs", true))
+			pop.play();
 	}
 
-	public void setTexture(Texture dot)
-	{
+	public void setTexture(Texture dot) {
 		this.dot = dot;
 	}
 
-	public Texture getTexture()
-	{
+	public Texture getTexture() {
 		return dot;
 	}
 
-	public void setPosition(float x, float y)
-	{
+	public void setPosition(float x, float y) {
 		setX(x);
 		setY(y);
 	}
 
 	// Overridden functions.
 	@Override
-	public void draw(Batch batch, float alpha)
-	{
-		batch.draw(dot, getX(), getY(), getCircleWidth(), getCircleHeight());
-//		dot.draw(batch, getX(), getY(), getCircleWidth(), getCircleHeight());
+	public void draw(Batch batch, float alpha) {
+		batch.draw(dot, getX(), getY(),
+				getCircleWidth() + random.nextInt(BUMP_IT_UP),
+				getCircleHeight() + random.nextInt(BUMP_IT_UP));
+		// dot.draw(batch, getX(), getY(), getCircleWidth(), getCircleHeight());
 	}
 }
