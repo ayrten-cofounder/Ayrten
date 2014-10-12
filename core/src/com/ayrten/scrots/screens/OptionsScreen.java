afb50a41804b5c66646dd2ea7fe2c01751053d47
Andy Yeung <nsdfer@gmail.com>
2014-10-11 19:23:25 -0700

    Nothing

--- core/src/com/ayrten/scrots/screens/OptionsScreen.java
+++ core/src/com/ayrten/scrots/screens/OptionsScreen.java
@@ -55,6 +55,7 @@ public class OptionsScreen implements Screen
 		sound_effs = new CheckBox("", game.skin);
 		sound_effs.setChecked(true);
 		sound_effs.getStyle().font = game.font_32;
+		sound_effs.getCells().get(0).size(game.font_32.getLineHeight()/2);
 		if(game.prefs.getBoolean("sound_effs", true) == false)
 			sound_effs.setChecked(false);
 		
