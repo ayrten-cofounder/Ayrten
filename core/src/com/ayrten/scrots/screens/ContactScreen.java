5cfcd4a5ad880b1477138a03ccec03752f65a84e
Tony Doan <ephemeraldreams89@gmail.com>
2014-12-20 15:22:56 -0800

    Fixed the wrapping of contact us screen

--- core/src/com/ayrten/scrots/screens/ContactScreen.java
+++ core/src/com/ayrten/scrots/screens/ContactScreen.java
@@ -17,7 +17,7 @@ public class ContactScreen extends ScrotsScreen
 	{
 		super(bscreen, false);
 		createBackLabel();
-		
+
 		Table table = new Table(Assets.skin);
 		table.left().top();
 		table.padLeft(back.getWidth()/5).padRight(back.getWidth()/5);
@@ -26,12 +26,12 @@ public class ContactScreen extends ScrotsScreen
 		msg.setText("Want to make a suggestion to improve the game? Got a bug to report? We at Ayrten strive to provide" +
 				" quick and responsive support. We would like to sincerely thank you for playing Scrots.");
 		msg.setWrap(true);
-		
+
 		Label general = new Label("", Assets.style_font_32_orange);
-		general.setText("Please send all inquiries/comments to ");
+		general.setText("Please send all inquiries/comments to: ");
 		general.setWrap(true);
-		general.setWidth(general.getStyle().font.getBounds("Please send all inquiries/comments to ").width);
-		
+		general.setWidth(general.getStyle().font.getBounds("Please send all inquiries/comments to: ").width);
+
 		Label generalURL = new Label("android.ayrten@gmail.com", Assets.style_font_32_blue);
 		generalURL.setWrap(true);
 		generalURL.setBounds(generalURL.getX(), generalURL.getY(), generalURL.getWidth(), generalURL.getHeight());
@@ -42,14 +42,21 @@ public class ContactScreen extends ScrotsScreen
 				Assets.game.apk_intf.copyTextToClipboard("android.ayrten@gmail.com");
 			}
 		});
-		
+
 		Table generalTable = new Table(Assets.skin);
-		generalTable.add(general).width(general.getWidth());
-		generalTable.add(generalURL).width(generalURL.getWidth());
-		
+		generalTable.left();
+		if(general.getWidth() + generalURL.getWidth() > Gdx.graphics.getWidth() - back.getWidth()/5 * 2) {
+			generalTable.add(general).width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2).left();
+			generalTable.row();
+			generalTable.add(generalURL).width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2);
+		} else {
+			generalTable.add(general).width(general.getWidth()).left();
+			generalTable.add(generalURL).width(generalURL.getWidth()).left();
+		}
+
 		Label facebook = new Label("Facebook: ", Assets.style_font_32_orange);
 		facebook.setWrap(true);
-		
+
 		Label facebookURL = new Label("https://www.facebook.com/AyrtenMobile", Assets.style_font_32_blue);
 		facebookURL.setBounds(facebookURL.getX(), facebookURL.getY(), facebookURL.getWidth(), facebookURL.getHeight());
 		facebookURL.setWrap(true);
@@ -59,14 +66,21 @@ public class ContactScreen extends ScrotsScreen
 				Gdx.net.openURI("https://www.facebook.com/AyrtenMobile");
 			}
 		});
-		
+
 		Table facebookTable = new Table(Assets.skin);
-		facebookTable.add(facebook).width(facebook.getWidth());
-		facebookTable.add(facebookURL).width(facebookURL.getWidth());
+		facebookTable.left();
+		if(facebook.getWidth() + facebookURL.getWidth() > Gdx.graphics.getWidth() - back.getWidth()/5 * 2) {
+			facebookTable.add(facebook).width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2).left();
+			facebookTable.row();
+			facebookTable.add(facebookURL).width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2);
+		} else {
+			facebookTable.add(facebook).width(facebook.getWidth());
+			facebookTable.add(facebookURL).width(facebookURL.getWidth()).left();
+		}
 		
 		Label twitter = new Label("Twitter: ", Assets.style_font_32_orange);
 		twitter.setWrap(true);
-		
+
 		Label twitterURL = new Label("https://twitter.com/AyrtenMobile", Assets.style_font_32_blue);
 		twitterURL.setWrap(true);
 		twitterURL.setBounds(twitterURL.getX(), twitterURL.getY(), twitterURL.getWidth(), twitterURL.getHeight());
@@ -76,10 +90,17 @@ public class ContactScreen extends ScrotsScreen
 				Gdx.net.openURI("https://twitter.com/AyrtenMobile");
 			}
 		});
-		
+
 		Table twitterTable = new Table(Assets.skin);
-		twitterTable.add(twitter).width(twitter.getWidth());
-		twitterTable.add(twitterURL).width(twitterURL.getWidth());
+		twitterTable.left();
+		if(twitter.getWidth() + twitterURL.getWidth() > Gdx.graphics.getWidth() - back.getWidth()/5 * 2) {
+			twitterTable.add(twitter).width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2).left();
+			twitterTable.row();
+			twitterTable.add(twitterURL).width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2);
+		} else {
+			twitterTable.add(twitter).width(twitter.getWidth()).left();
+			twitterTable.add(twitterURL).width(twitterURL.getWidth()).left();
+		}
 		
 		table.add(back).left();
 		table.row();
@@ -89,21 +110,21 @@ public class ContactScreen extends ScrotsScreen
 		table.row();
 		table.add("").height(back.getStyle().font.getLineHeight()/2);
 		table.row();
-		table.add(generalTable).width(Gdx.graphics.getWidth() - back.getWidth()/5);
+		table.add(generalTable).width(Gdx.graphics.getWidth() - back.getWidth()/5).left();
 		table.row();
 		table.add("").height(back.getStyle().font.getLineHeight()/2);
 		table.row();
-		table.add(facebookTable).left().width(Gdx.graphics.getWidth() - back.getWidth()/5);
+		table.add(facebookTable).left().width(Gdx.graphics.getWidth() - back.getWidth()/5).left();
 		table.row();
 		table.add("").height(back.getStyle().font.getLineHeight()/2);
 		table.row();
-		table.add(twitterTable).left().width(Gdx.graphics.getWidth() - back.getWidth()/5);
-		
+		table.add(twitterTable).left().width(Gdx.graphics.getWidth() - back.getWidth()/5).left();
+
 		scroll_view = new ScrollPane(table);
 		scroll_view.setFillParent(true);
 		scroll_view.setFlickScroll(true);
 		scroll_view.updateVisualScroll();
-		
+
 		setupStage();
 		stage.addActor(scroll_view);
 	}
