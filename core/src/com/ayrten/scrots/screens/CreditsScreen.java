e8a81c5f9bff652281c337787af73d1b50d14bf6
Andy Yeung <nsdfer@gmail.com>
2015-03-22 15:47:10 -0700

    Changed UI
    All screens should have the same table and navagation bar

--- core/src/com/ayrten/scrots/screens/CreditsScreen.java
+++ core/src/com/ayrten/scrots/screens/CreditsScreen.java
@@ -11,140 +11,157 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-public class CreditsScreen extends ScrotsScreen 
-{	
-	public CreditsScreen(Screen bscreen) 
-	{
+public class CreditsScreen extends ScrotsScreen {
+	public CreditsScreen(Screen bscreen) {
 		super(bscreen, true);
 
 		setupStage();
 		showTableScreen();
-		
+
 		Label created = new Label("", Assets.style_font_32_orange);
 		created.setWrap(true);
 		created.setText("Created by");
-		created.setWidth(Assets.style_font_32_orange.font.getBounds("Created by").width);
-		
+		created.setWidth(Assets.style_font_32_orange.font
+				.getBounds("Created by").width);
+
 		Label createdNames = new Label("", Assets.style_font_32_orange);
 		createdNames.setWrap(true);
 		createdNames.setText("Andy Yeung and Tony Doan");
-		
+
 		Label thanks = new Label("", Assets.style_font_32_orange);
 		thanks.setWrap(true);
 		thanks.setText("Special thanks to");
-		thanks.setWidth(Assets.style_font_32_orange.font.getBounds("Special thanks to").width);
-		
+		thanks.setWidth(Assets.style_font_32_orange.font
+				.getBounds("Special thanks to").width);
+
 		Label thanksName = new Label("", Assets.style_font_32_orange);
 		thanksName.setWrap(true);
 		thanksName.setText("Family and friends who supported us");
-		thanksName.setWidth(Assets.style_font_32_orange.font.getBounds("Family and friends who supported us").width);
-		
+		thanksName.setWidth(Assets.style_font_32_orange.font
+				.getBounds("Family and friends who supported us").width);
+
 		ArrayList<String> thanks_list = new ArrayList<String>();
 		thanks_list.add("Trina Dao");
 		thanks_list.add("Emily Do");
-		
+
 		Table thanks_table = new Table(Assets.skin);
 		thanks_table.left();
 		thanks_table.add(thanksName);
-		for(int i = 0; i < thanks_list.size(); i++)
-		{
-			Label thank_person = new Label(thanks_list.get(i), Assets.style_font_32_orange);
+		for (int i = 0; i < thanks_list.size(); i++) {
+			Label thank_person = new Label(thanks_list.get(i),
+					Assets.style_font_32_orange);
 			thank_person.setWrap(true);
-			thank_person.setWidth(Assets.style_font_32_orange.font.getBounds(thanks_list.get(i)).width);
+			thank_person.setWidth(Assets.style_font_32_orange.font
+					.getBounds(thanks_list.get(i)).width);
 			thanks_table.row();
 			thanks_table.add(thank_person);
 		}
-		
+
 		Label publish = new Label("", Assets.style_font_32_orange);
 		publish.setWrap(true);
 		publish.setText("Published by");
-		publish.setWidth(Assets.style_font_32_orange.font.getBounds("Published by").width);
-		
+		publish.setWidth(Assets.style_font_32_orange.font
+				.getBounds("Published by").width);
+
 		Label publishName = new Label("", Assets.style_font_32_orange);
 		publishName.setWrap(true);
 		publishName.setText("Ayrten");
-		publishName.setWidth(Assets.style_font_32_orange.font.getBounds("Ayrten").width);
-		
+		publishName.setWidth(Assets.style_font_32_orange.font
+				.getBounds("Ayrten").width);
+
 		Label misc = new Label("", Assets.style_font_32_orange);
 		misc.setWrap(true);
 		misc.setText("Assets are used according to terms described in the Creative Commons 3.0 Attribution"
 				+ " License. Giving credit and thanks to the following:");
-		misc.setBounds(misc.getX(), misc.getY(), misc.getWidth(), misc.getHeight());
-		
-		Label miscMem0 = new Label("http://audionautix.com", Assets.style_font_32_blue);
+		misc.setBounds(misc.getX(), misc.getY(), misc.getWidth(),
+				misc.getHeight());
+
+		Label miscMem0 = new Label("http://audionautix.com",
+				Assets.style_font_32_blue);
 		miscMem0.setWrap(true);
-		miscMem0.setBounds(miscMem0.getX(), miscMem0.getY(), miscMem0.getWidth(), miscMem0.getHeight());
+		miscMem0.setBounds(miscMem0.getX(), miscMem0.getY(),
+				miscMem0.getWidth(), miscMem0.getHeight());
 		miscMem0.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				Gdx.net.openURI("http://audionautix.com");
 			}
 		});
-		
-		Label miscMem1 = new Label("http://www.bensound.com", Assets.style_font_32_blue);
+
+		Label miscMem1 = new Label("http://www.bensound.com",
+				Assets.style_font_32_blue);
 		miscMem1.setWrap(true);
-		miscMem1.setBounds(miscMem1.getX(), miscMem1.getY(), miscMem1.getWidth(), miscMem1.getHeight());
+		miscMem1.setBounds(miscMem1.getX(), miscMem1.getY(),
+				miscMem1.getWidth(), miscMem1.getHeight());
 		miscMem1.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				Gdx.net.openURI("http://www.bensound.com");
 			}
 		});
-		
-		Label miscMem2 = new Label("https://www.freesound.org", Assets.style_font_32_blue);
+
+		Label miscMem2 = new Label("https://www.freesound.org",
+				Assets.style_font_32_blue);
 		miscMem2.setWrap(true);
-		miscMem2.setBounds(miscMem2.getX(), miscMem2.getY(), miscMem2.getWidth(), miscMem2.getHeight());
-		miscMem2.addListener(new ClickListener(){
+		miscMem2.setBounds(miscMem2.getX(), miscMem2.getY(),
+				miscMem2.getWidth(), miscMem2.getHeight());
+		miscMem2.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				Gdx.net.openURI("https://www.freesound.org");
 			}
 		});
-		
-		Table table = new Table(Assets.skin);
-		table.left().top();
-		table.padLeft(back.getWidth()/5).padRight(back.getWidth()/5);
-		
+
 		Table upperTable = new Table(Assets.skin);
-		upperTable.add(back).left();
-		upperTable.row();
-		upperTable.add(created).left().top().padLeft(Gdx.graphics.getWidth()/2 - created.getWidth()).width(created.getWidth());
-		upperTable.add(createdNames).left().padLeft(back.getWidth()/5).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5 * 2);
-		upperTable.row();
-		upperTable.add("").height(back.getStyle().font.getLineHeight());
-		upperTable.row();
-		upperTable.add(thanks).left().top().padLeft(Gdx.graphics.getWidth()/2 - thanks.getWidth());
-		upperTable.add(thanks_table).left().padLeft(back.getWidth()/5).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5 * 2);
+		upperTable.add(created).left().top()
+				.padLeft(Gdx.graphics.getWidth() / 2 - created.getWidth())
+				.width(created.getWidth());
+		upperTable.add(createdNames).left().padLeft(back.getWidth() / 5)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5 * 2);
 		upperTable.row();
 		upperTable.add("").height(back.getStyle().font.getLineHeight());
 		upperTable.row();
-		upperTable.add(publish).left().top().padLeft(Gdx.graphics.getWidth()/2 - publish.getWidth());
-		upperTable.add(publishName).left().padLeft(back.getWidth()/5).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5);
-		
+		upperTable.add(thanks).left().top()
+				.padLeft(Gdx.graphics.getWidth() / 2 - thanks.getWidth());
+		upperTable.add(thanks_table).left().padLeft(back.getWidth() / 5)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5 * 2);
+		upperTable.row();
+		upperTable.add("").height(back.getStyle().font.getLineHeight());
+		upperTable.row();
+		upperTable.add(publish).left().top()
+				.padLeft(Gdx.graphics.getWidth() / 2 - publish.getWidth());
+		upperTable.add(publishName).left().padLeft(back.getWidth() / 5)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5);
+
 		Table bottomTable = new Table(Assets.skin);
 		bottomTable.left().top();
-		bottomTable.add(misc).left().width(Gdx.graphics.getWidth() - back.getWidth()/5);
+		bottomTable.add(misc).left()
+				.width(Gdx.graphics.getWidth() - back.getWidth() / 5);
 		bottomTable.row();
-		bottomTable.add(miscMem0).left().width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2);
+		bottomTable.add(miscMem0).left()
+				.width(Gdx.graphics.getWidth() - back.getWidth() / 5 * 2);
 		bottomTable.row();
-		bottomTable.add(miscMem1).left().width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2);
+		bottomTable.add(miscMem1).left()
+				.width(Gdx.graphics.getWidth() - back.getWidth() / 5 * 2);
 		bottomTable.row();
-		bottomTable.add(miscMem2).left().width(Gdx.graphics.getWidth() - back.getWidth()/5 * 2);
+		bottomTable.add(miscMem2).left()
+				.width(Gdx.graphics.getWidth() - back.getWidth() / 5 * 2);
 		bottomTable.row();
 		bottomTable.add("").height(back.getStyle().font.getLineHeight());
-		
-		
-		table.add(upperTable);
-		table.row();
-		table.add("").height(back.getStyle().font.getLineHeight());
-		table.row();
-		table.add(bottomTable);
-		
-		ScrollPane scroll_view = new ScrollPane(table);
-		scroll_view.setFillParent(true);
+
+		Table tempt = new Table(Assets.skin);
+		tempt.left().top();
+		tempt.padLeft(back.getWidth() / 5).padRight(back.getWidth() / 5);
+
+		tempt.add(upperTable);
+		tempt.row();
+		tempt.add("").height(back.getStyle().font.getLineHeight());
+		tempt.row();
+		tempt.add(bottomTable);
+
+		ScrollPane scroll_view = new ScrollPane(tempt);
 		scroll_view.setScrollingDisabled(true, false);
-		
-		setupStage();
-		stage.addActor(scroll_view);
+
+		table.add(scroll_view);
 	}
 }
--- core/src/com/ayrten/scrots/screens/TutorialScreen.java
+++ core/src/com/ayrten/scrots/screens/TutorialScreen.java
@@ -10,104 +10,114 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-public class TutorialScreen extends ScrotsScreen
-{	
-	public TutorialScreen(Screen bscreen)
-	{
-		super(bscreen, false);
-		createBackLabelAndInitNavBar();
-		
-		Table table = new Table(Assets.skin);
-		table.padLeft(back.getWidth()/5).padRight(back.getWidth()/5);
-		
+public class TutorialScreen extends ScrotsScreen {
+	public TutorialScreen(Screen bscreen) {
+		super(bscreen, true);
+
+		setupStage();
+		showTableScreen();
+
 		Label intent = new Label("", Assets.style_font_32_orange);
 		intent.setWrap(true);
 		intent.setText("The goal of the game is pop all the greens on screen as fast"
 				+ " as you can! When all green dots are popped, you gain a point and"
 				+ " the difficulty level increases. If time runs out or you popped a red"
 				+ " dot, then GAME OVER! Below are the type of dots you'll be encountering:");
-				
+
 		Image greenDot = new Image(Assets.regDot_1);
 		Image redDot = new Image(Assets.penDot_1);
 		Image blueDot = new Image(Assets.penDot_2);
 		Image babyBlueDot = new Image(Assets.regDot_2);
 		Image questionDot = new Image(Assets.question_mark);
-		
+
 		Label greenDesc = new Label("", Assets.style_font_32_orange);
 		greenDesc.setWrap(true);
 		greenDesc.setText("Requires popping in order to advance");
-		
+
 		Label redDesc = new Label("", Assets.style_font_32_orange);
-		
+
 		redDesc.setWrap(true);
 		redDesc.setText("Immediate GAME OVER!");
-		
+
 		Label blueDesc = new Label("", Assets.style_font_32_orange);
 		blueDesc.setWrap(true);
 		blueDesc.setText("Decreases time limit by 5 seconds so BE CAREFUL!");
-		
+
 		Label babyBlueDesc = new Label("", Assets.style_font_32_orange);
 		babyBlueDesc.setWrap(true);
 		babyBlueDesc.setText("Increased time limit by 2.5 seconds");
-		babyBlueDesc.addListener(new ClickListener(){
+		babyBlueDesc.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				// TODO Auto-generated method stub
 				super.clicked(event, x, y);
 			}
 		});
-		
+
 		Label questionDesc = new Label("", Assets.style_font_32_orange);
 		questionDesc.setWrap(true);
 		questionDesc.setText("More to come!");
-		
+
 		Table innerTable = new Table(Assets.skin);
 		innerTable.left().top();
-		innerTable.add(greenDot).width(greenDot.getWidth()).height(greenDot.getHeight());
-		innerTable.add(greenDesc).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5 * 2).padLeft(back.getWidth()/5);
+		innerTable.add(greenDot).width(greenDot.getWidth())
+				.height(greenDot.getHeight());
+		innerTable.add(greenDesc)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5 * 2)
+				.padLeft(back.getWidth() / 5);
 		innerTable.row();
 		innerTable.add("").height(back.getStyle().font.getLineHeight());
 		innerTable.row();
-		innerTable.add(redDot).width(redDot.getWidth()).height(redDot.getHeight());
-		innerTable.add(redDesc).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5 * 2).padLeft(back.getWidth()/5);
+		innerTable.add(redDot).width(redDot.getWidth())
+				.height(redDot.getHeight());
+		innerTable.add(redDesc)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5 * 2)
+				.padLeft(back.getWidth() / 5);
 		innerTable.row();
 		innerTable.add("").height(back.getStyle().font.getLineHeight());
 		innerTable.row();
-		innerTable.add(blueDot).width(blueDot.getWidth()).height(blueDot.getHeight());
-		innerTable.add(blueDesc).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5 * 2).padLeft(back.getWidth()/5);
+		innerTable.add(blueDot).width(blueDot.getWidth())
+				.height(blueDot.getHeight());
+		innerTable.add(blueDesc)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5 * 2)
+				.padLeft(back.getWidth() / 5);
 		innerTable.row();
 		innerTable.add("").height(back.getStyle().font.getLineHeight());
 		innerTable.row();
-		innerTable.add(babyBlueDot).width(babyBlueDot.getWidth()).height(babyBlueDot.getHeight());
-		innerTable.add(babyBlueDesc).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5 * 2).padLeft(back.getWidth()/5);
+		innerTable.add(babyBlueDot).width(babyBlueDot.getWidth())
+				.height(babyBlueDot.getHeight());
+		innerTable.add(babyBlueDesc)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5 * 2)
+				.padLeft(back.getWidth() / 5);
 		innerTable.row();
 		innerTable.add("").height(back.getStyle().font.getLineHeight());
 		innerTable.row();
-		innerTable.add(questionDot).width(questionDot.getWidth()).height(questionDot.getHeight());
-		innerTable.add(questionDesc).width(Gdx.graphics.getWidth()/2 - back.getWidth()/5 * 2).padLeft(back.getWidth()/5);
-		
-		table.add(back).left();
-		table.row();
-		table.add(intent).width(Gdx.graphics.getWidth() - back.getWidth()/5);
-		table.row();
-		table.add("").height(back.getStyle().font.getLineHeight());
-		table.row();
-		table.add(innerTable);
-		table.row();
-		table.add("").height(intent.getStyle().font.getLineHeight());
-		
-		ScrollPane scroll_view = new ScrollPane(table);
-		scroll_view.setFillParent(true);
+		innerTable.add(questionDot).width(questionDot.getWidth())
+				.height(questionDot.getHeight());
+		innerTable.add(questionDesc)
+				.width(Gdx.graphics.getWidth() / 2 - back.getWidth() / 5 * 2)
+				.padLeft(back.getWidth() / 5);
+
+		Table tempt = new Table(Assets.skin);
+
+		tempt.add(intent).width(table.getWidth() - 100);
+		tempt.row();
+		tempt.add("").height(back.getStyle().font.getLineHeight());
+		tempt.row();
+		tempt.add(innerTable);
+		tempt.row();
+		tempt.add("").height(intent.getStyle().font.getLineHeight());
+
+		ScrollPane scroll_view = new ScrollPane(tempt);
 		scroll_view.setScrollingDisabled(true, false);
-		
-		setupStage();
-		stage.addActor(scroll_view);
+
+		table.add(scroll_view);
 	}
-	
+
 	@Override
 	public void hide() {
 		super.hide();
-		if(Assets.prefs.getBoolean("first_time", true)) {
+		if (Assets.prefs.getBoolean("first_time", true)) {
 			Assets.prefs.putBoolean("first_time", false);
 			Assets.prefs.flush();
 		}
